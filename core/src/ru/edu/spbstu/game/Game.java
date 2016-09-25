44c070b7b44de3f7ccf0a8429447eb200ad11ffb
Admin <ilyamorzharetto@icloud.com>
2016-09-25 14:32:58 +0300

    Deleted redundant test; crossroads on mapArray now marked by "2"; Rendering map using "ShapeRenderer"

--- core/src/ru/edu/spbstu/game/Game.java
+++ core/src/ru/edu/spbstu/game/Game.java
@@ -3,26 +3,60 @@ package ru.edu.spbstu.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+    ShapeRenderer shapeRenderer;
+    OrthographicCamera camera;
+    Map map;
+    int width = 800;
+    int height = 480;
+    int tileWidth = 20;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        shapeRenderer = new ShapeRenderer();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, width, height);
+        shapeRenderer.setProjectionMatrix(camera.combined);
+		map = new Map(width, height);
+        map.generate();
+        for(int i=0; i<map.mapArray.length; ++i) {
+            for (int j = 0; j < map.mapArray[i].length; ++j) {
+                System.out.print(map.mapArray[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+        camera.update();
+        int xBrush = 0;
+        int yBrush = 0;
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 1, 1, 1);
+        for (int i=(map.height-1); i>=0; --i) {
+            for(int j=0; j<map.width; ++j) {
+                if (map.mapArray[i][j] == 1 || map.mapArray[i][j] == 2) {
+                    shapeRenderer.rect(xBrush, yBrush, tileWidth, tileWidth);
+                }
+                xBrush += tileWidth;
+            }
+            yBrush += tileWidth;
+            xBrush = 0;
+        }
+        shapeRenderer.end();
 	}
 	
 	@Override
--- core/src/ru/edu/spbstu/game/Map.java
+++ core/src/ru/edu/spbstu/game/Map.java
@@ -3,17 +3,21 @@ import java.util.*;
 
 public class Map {
 
-    public int[][] mapArray = new int[40][30];
+    public int[][] mapArray = new int[30][40];
+    public int width = 40;
+    public int height = 30;
 
     public Map(){}
 
     public Map(int width, int height) {
-        mapArray = new int[width/20][height/20];
+        mapArray = new int[height/20][width/20];
+        this.width = width/20;
+        this.height = height/20;
     }
 
 
     public void generate() {
-        Random rand = new Random(42);
+        Random rand = new Random();
         int i = 1 + rand.nextInt(2);
         while (i < mapArray.length - 1) {
             for(int j = 0; j < mapArray[i].length; ++j) {
@@ -24,7 +28,12 @@ public class Map {
         i = 1 + rand.nextInt(2);
         while (i < mapArray[0].length - 1) {
             for (int j=0; j < mapArray.length; ++j) {
-                mapArray[j][i] = 1;
+                if (mapArray[j][i] == 1) {
+                    mapArray[j][i] = 2;
+                }
+                else {
+                    mapArray[j][i] = 1;
+                }
             }
             i += 2 + rand.nextInt(4);
         }
